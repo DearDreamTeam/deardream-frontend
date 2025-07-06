f4e52a524e209c00faf66d18dbe8419a26942f50
Yeongseo Kim <1903020@ewhain.net>
2025-07-07 02:49:09 +0900

    chore: letters 동적 경로 레이아웃 컴포넌트 삭제

--- src/app/letters/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-const PdfLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div className="bg-grey-500 h-full">{children}</div>;
-};
-
-export default PdfLayout;
--- src/app/letters/[id]/page.tsx
+++ src/app/letters/[id]/page.tsx
@@ -7,7 +7,7 @@ import PdfViewer from "./_components/pdf-viewer";
 const PdfPage = () => {
   const pdfSrc = "/mock/1.pdf";
   return (
-    <div>
+    <div className="bg-grey-500 h-full">
       <header className="text-title-1 header justify-between py-[0.66rem]">
         <div className="flex items-center">
           <Link href={PATH.LETTER_LIST}>
