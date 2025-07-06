704b43eccc6810d25a846a3c6e26e31a73946b7d
Yeongseo Kim <1903020@ewhain.net>
2025-07-06 17:17:09 +0900

    style: gnb 디자인 수정

--- src/components/gnb/navigation-bar.tsx
+++ src/components/gnb/navigation-bar.tsx
@@ -13,12 +13,12 @@ const NavigationBar = () => {
   if (pathname.startsWith("/letter/") || pathname.startsWith("/letters/"))
     return null;
   return (
-    <nav className="text-body-2 bg-grey-0 shadow-default flex justify-evenly gap-12 px-4 py-[1.29rem]">
+    <nav className="text-body-1 bg-grey-0 shadow-default flex justify-evenly gap-12 px-4 py-[1.29rem]">
       {NAV_ITEMS.map(({ icon: Icon, label, href }) => (
         <Link
           href={href}
           key={label}
-          className={pathname === href ? "text-grey-700" : "text-grey-300"}
+          className={pathname === href ? "text-grey-700" : "text-grey-400"}
         >
           <NavigationItem>
             <Icon />
