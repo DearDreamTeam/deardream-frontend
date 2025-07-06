193dce69f701640e2174fdad5b775dd96d3adad3
Yeongseo Kim <1903020@ewhain.net>
2025-07-06 18:59:34 +0900

    feat: 랜딩, 온보딩 페이지 구현

---
+++ src/components/landing/landing.tsx
@@ -0,0 +1,10 @@
+import LogoBig from "@/public/logo/logo-big.svg";
+const Landing = () => {
+  return (
+    <div className="text-grey-0 flex h-full w-full items-center justify-center bg-green-300 p-14">
+      <LogoBig />
+    </div>
+  );
+};
+
+export default Landing;
