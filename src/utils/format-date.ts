00dd7824b03b8a42322316deb0012ba74286eca6
Yeongseo Kim <1903020@ewhain.net>
2025-07-12 19:46:33 +0900

    feat: 시간 포맷 시간까지 나오도록 변경

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -5,7 +5,14 @@ export const formatDateToDots = (timestamp: string) => {
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
 
-  return `${year}.${month}.${day}`;
+  let hour = date.getHours();
+  const minutes = date.getMinutes();
+
+  const isAm = hour < 12;
+  const period = isAm ? "오전" : "오후";
+  hour = isAm ? hour : hour - 12;
+
+  return `${year}.${month}.${day} ${period} ${hour}:${minutes}`;
 };
 
 export const formatDateForNewsletter = (timestamp: number) => {
