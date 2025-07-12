8a8b0a1008ded9556a8c2c2b555284f56c6e3a96
Yeongseo Kim <1903020@ewhain.net>
2025-07-12 11:31:56 +0900

    chore: 메타 태그 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,22 @@ export const metadata: Metadata = {
   title: "이어드림",
   description:
     "사랑하는 가족들의 소식을 받으며 웃음짓는 건강한 세상을 만듭니다.",
-  authors: [{ name: "Yeongseo Kim" }, { name: "Juhee Lee" }],
+  keywords: [
+    "이어드림",
+    "소식지",
+    "소식지 서비스",
+    "요양원",
+    "부모님",
+    "가족",
+    "아기 사진",
+    "포토북",
+    "사진북",
+  ],
+  authors: [{ name: "DearDream", url: "https://www.deardream.site" }],
+  creator: "DearDream",
+  publisher: "DearDream",
+  category: "social",
+  applicationName: "이어드림",
   icons: {
     icon: "/images/ribbon/ribbon-full.svg",
   },
@@ -24,9 +39,20 @@ export const metadata: Metadata = {
     title: "이어드림",
     description:
       "사랑하는 가족들의 소식을 받으며 웃음짓는 건강한 세상을 만듭니다.",
-    url: "",
+    url: "https://www.deardream.site",
+    locale: "ko-KR",
     siteName: "이어드림",
     type: "website",
+    countryName: "대한민국",
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1566125882500-87e10f726cdc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        width: 620,
+        height: 465,
+        alt: "이어드림, 가족의 소식을 한 권의 책으로 전하는 정서 연결 서비스",
+        type: "image/png",
+      },
+    ],
   },
 };
 
