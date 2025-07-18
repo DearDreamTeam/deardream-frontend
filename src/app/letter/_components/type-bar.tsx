bb4621271552608b18600797052c3c34805db865
Yeongseo Kim <1903020@ewhain.net>
2025-07-18 16:59:33 +0900

    refactor: restructure letter, new, edit for protected routes

--- src/app/letter/_components/type-bar.tsx
+++
@@ -1,9 +0,0 @@
-const TypeBar = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="bg-grey-0 border-y-grey-200 flex items-center justify-between border-y px-5 py-4">
-      {children}
-    </div>
-  );
-};
-
-export default TypeBar;
