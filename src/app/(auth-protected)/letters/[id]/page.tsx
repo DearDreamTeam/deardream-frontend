ffe0555943b130b2a02bba20fa5850d573c03041
Yeongseo Kim <1903020@ewhain.net>
2025-07-19 16:53:08 +0900

    refactor: download icon 위치 변경

--- src/app/(auth-protected)/letters/[id]/page.tsx
+++ src/app/(auth-protected)/letters/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { PATH } from "@/constants/path";
 import ArrowBack from "@/public/icons/common/arrow-back.svg";
-import Download from "@/public/icons/letters/download.svg";
+import Download from "@/public/icons/common/download.svg";
 import Link from "next/link";
 import PdfViewer from "@/components/pdf-viewer/pdf-viewer";
 
@@ -10,7 +10,7 @@ const PdfPage = () => {
     <div className="bg-grey-500 h-full">
       <header className="text-title-1 header justify-between py-[0.66rem]">
         <div className="flex items-center">
-          <Link href={PATH.LETTER_LIST}>
+          <Link href={PATH.LETTER_LIST} className="text-grey-600">
             <ArrowBack />
           </Link>
           <h1>2025년 5월호</h1>
