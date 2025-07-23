51fe9955d8e1f7c0eb256452b516834e45973a40
Yeongseo Kim <1903020@ewhain.net>
2025-07-23 13:07:51 +0900

    style: period-notification rounded 적용

--- src/app/(auth-protected)/home/_components/period-notification.tsx
+++ src/app/(auth-protected)/home/_components/period-notification.tsx
@@ -5,7 +5,7 @@ const PeriodNotification = () => {
   const lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   return (
     <div className="px-4 py-2">
-      <div className="flex w-full items-center justify-center gap-1 bg-green-100 px-4 text-green-300">
+      <div className="flex w-full items-center justify-center gap-1 rounded-sm bg-green-100 px-4 text-green-300">
         <Notification />
         <small className="text-caption-1">
           오늘부터 {lastDate.getMonth() + 1}월 {lastDate.getDate()}일까지
