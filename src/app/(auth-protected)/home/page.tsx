cccbc47b1a1d79dbd6f63563e03e9708ed519460
Yeongseo Kim <1903020@ewhain.net>
2025-07-24 15:47:13 +0900

    feat: pull-to-refresh 적용

--- src/app/(auth-protected)/home/page.tsx
+++ src/app/(auth-protected)/home/page.tsx
@@ -1,19 +1,33 @@
 "use client";
+import { useEffect, useState } from "react";
 import { usePostStore } from "@/stores/usePostStore";
-import NoPost from "./_components/no-post";
-import Postcard from "@/components/postcard/postcard";
 import { useUserStore } from "@/stores/useUserInfoStore";
-import NoFamilyGroup from "./_components/no-family-group";
+
 import HomeBanner from "./_components/home-banner";
 import PeriodNotification from "./_components/period-notification";
-import { useEffect, useState } from "react";
-import { getFamilyPosts } from "@/api/post";
+import NoPost from "./_components/no-post";
+import NoFamilyGroup from "./_components/no-family-group";
+import Postcard from "@/components/postcard/postcard";
 import PostcardSkeleton from "@/components/skeleton/postcard-skeleton";
+import dynamic from "next/dynamic";
+
+const PullToRefresh = dynamic(() => import("react-pull-to-refresh"), {
+  ssr: false,
+});
+
+import { getFamilyPosts } from "@/api/post";
 
 const Home = () => {
   const { post, setPost } = usePostStore();
   const { userProfile } = useUserStore();
   const [isLoading, setIsLoading] = useState(false);
+
+  const fetchFamilyPosts = async () => {
+    if (userProfile.familyId) {
+      const posts = await getFamilyPosts(userProfile.familyId);
+      setPost(posts);
+    }
+  };
 
   useEffect(() => {
     if (userProfile.familyId) {
@@ -34,22 +48,24 @@ const Home = () => {
     <div className="overflow-auto-hide-scroll h-full">
       <HomeBanner />
       <PeriodNotification />
-      {post
-        .slice()
-        .reverse()
-        .map((letter) => (
-          <Postcard
-            authorId={letter.authorId}
-            key={letter.postId}
-            postId={letter.postId}
-            authorName={letter.authorName}
-            relations={letter.relations}
-            authorProfileImg={letter.authorProfileImg}
-            createdAt={letter.createdAt}
-            content={letter.content}
-            imageUrls={letter.imageUrls}
-          />
-        ))}
+      <PullToRefresh onRefresh={fetchFamilyPosts} className="h-full">
+        {post
+          .slice()
+          .reverse()
+          .map((letter) => (
+            <Postcard
+              authorId={letter.authorId}
+              key={letter.postId}
+              postId={letter.postId}
+              authorName={letter.authorName}
+              relations={letter.relations}
+              authorProfileImg={letter.authorProfileImg}
+              createdAt={letter.createdAt}
+              content={letter.content}
+              imageUrls={letter.imageUrls}
+            />
+          ))}
+      </PullToRefresh>
     </div>
   );
 };
