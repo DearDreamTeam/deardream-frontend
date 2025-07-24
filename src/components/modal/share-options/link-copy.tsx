b062ce54d6cdeca367fd943980148159111b2fe1
Yeongseo Kim <1903020@ewhain.net>
2025-07-24 17:28:21 +0900

    fix: family link line-clamp 적용

--- src/components/modal/share-options/link-copy.tsx
+++ src/components/modal/share-options/link-copy.tsx
@@ -24,7 +24,7 @@ const LinkCopy = () => {
     <div className="text-grey-500 flex flex-col">
       <p className="text-body-2 p-1 text-center">또는 링크 복사하기</p>
       <div className="bg-grey-50 flex gap-1 rounded-sm px-4 py-[0.69rem]">
-        <span className="flex-1">
+        <span className="line-clamp-1 flex-1">
           {(SHARE_DATA.url ?? "") + "invite?familylink=" + (familyLink ?? "")}
         </span>
         <button onClick={handleCopy}>
