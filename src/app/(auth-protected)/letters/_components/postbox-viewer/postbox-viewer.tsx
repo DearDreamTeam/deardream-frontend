5d699f79ec52c93d5196bfb6aa9e4ad8a3f7321c
Yeongseo Kim <1903020@ewhain.net>
2025-07-27 15:41:41 +0900

    feat: 가족 그룹 없을 시 소식함 서비스 페이지

--- src/app/(auth-protected)/letters/_components/postbox-viewer/postbox-viewer.tsx
+++ src/app/(auth-protected)/letters/_components/postbox-viewer/postbox-viewer.tsx
@@ -5,11 +5,15 @@ import PostList from "../post-list/post-list";
 import FilterBar from "../filter-bar/filter-bar";
 import { SortKey } from "../letters-props";
 import { useLettersStore } from "@/stores/useLettersStore";
+import { useUserStore } from "@/stores/useUserInfoStore";
+import NoFamilyGroup from "./no-family-group";
 
 const PostboxViewer = () => {
+  const { userProfile } = useUserStore();
   const [showOnlyFavorites, setShowOnlyFavorites] = useState(false);
   const [sortOption, setSortOption] = useState<SortKey>(null);
 
+  if (userProfile.familyId === null) return <NoFamilyGroup />;
   return (
     <div className="overflow-auto-hide-scroll px-4">
       <FilterBar
