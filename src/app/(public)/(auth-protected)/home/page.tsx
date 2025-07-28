02cd6099148f5b6fcdcabd3a22faa971b55b6d47
Yeongseo Kim <88617509+kkys00@users.noreply.github.com>
2025-07-28 15:06:17 +0900

    Fix: mobile scroll (#68)
    * fix: mobile에서 스크롤되지 않는 문제 수정
    
    * fix: 잘못된 이미지 선택 방식 수정
    
    * fix: 모바일 스크롤 오류 해결

--- src/app/(public)/(auth-protected)/home/page.tsx
+++ src/app/(public)/(auth-protected)/home/page.tsx
@@ -48,6 +48,11 @@ const Home = () => {
     <PullToRefresh
       onRefresh={fetchFamilyPosts}
       className="overflow-auto-hide-scroll h-full"
+
+      style={{
+        touchAction: "pan-y",
+        WebkitOverflowScrolling: "touch",
+      }}
     >
       <HomeBanner />
       <PeriodNotification />
