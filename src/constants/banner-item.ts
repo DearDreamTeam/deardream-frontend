3ef16aeef6cdf8d18ccabe60f1e11f526872e1c6
leejuhi <anim0203@naver.com>
2025-07-29 10:05:04 +0900

    feat: 배너 아이템 path 처리

--- src/constants/banner-item.ts
+++ src/constants/banner-item.ts
@@ -4,16 +4,16 @@ export const BANNER_ITEM = [
   {
     title: "소식 작성 가이드",
     content: "어떤 소식을 남겨야할지 막막하시다고요?",
-    href: PATH.MYPAGE,
+    href: PATH.MYPAGE + "/guide/letter",
   },
   {
     title: "소식 작성 마감 가이드",
     content: "홈 화면의 소식들이 전부 사라지셨다고요?",
-    href: PATH.MYPAGE,
+    href: PATH.MYPAGE + "/guide/write",
   },
   {
     title: "주소 이전 가이드 A to Z",
     content: "기관을 옮기셨나요? 걱정하지 마세요!",
-    href: PATH.MYPAGE,
+    href: PATH.MYPAGE + "/guide/address",
   },
 ];
