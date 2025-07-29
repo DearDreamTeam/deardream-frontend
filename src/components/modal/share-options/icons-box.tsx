a9d641e9301395ef7b2b262db00e116b338b47d5
Yeongseo Kim <88617509+kkys00@users.noreply.github.com>
2025-07-29 19:50:40 +0900

    fix: share link 연결 (#76)

--- src/components/modal/share-options/icons-box.tsx
+++ src/components/modal/share-options/icons-box.tsx
@@ -2,12 +2,19 @@
 
 import { SHARE_OPTION_ITEMS } from "@/components/modal/share-options/share-option-items";
 import IconItem from "./icon-item";
+import { useInvitationStore } from "@/stores/useInvitationStore";
 
 const IconsBox = () => {
+  const { familyLink } = useInvitationStore();
   return (
     <div className="text-grey-700 flex items-center justify-around gap-6 py-4">
       {SHARE_OPTION_ITEMS.map(({ icon: Icon, label, action }) => (
-        <IconItem key={label} icon={<Icon />} label={label} action={action} />
+        <IconItem
+          key={label}
+          icon={<Icon />}
+          label={label}
+          action={() => action(familyLink ?? "")}
+        />
       ))}
     </div>
   );
